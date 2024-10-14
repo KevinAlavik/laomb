dfa9ca0f23767a3ea1ef08257a995945f41f3034
NotBonzo <145292897+notbonzo-com@users.noreply.github.com>
2024-10-14 21:46:49 +0200

    Added liballoc + heap and introduced a wierd bug where memcpy somehow enables interrupts

--- kernel/src/_start.c
+++ kernel/src/_start.c
@@ -4,6 +4,7 @@
 #include <io.h>
 #include <string.h>
 #include <ultra_protocol.h>
+#include <kheap.h>
 
 uintptr_t higher_half_base;
 
@@ -28,8 +29,35 @@ struct ultra_framebuffer_attribute* framebuffer = NULL;
     idt_init();
     pmm_init(ctx);
 
+    struct ultra_attribute_header* head = ctx->attributes;
+    for (size_t i = 0; i < ctx->attribute_count; i++, head = ULTRA_NEXT_ATTRIBUTE(head)) {
+        switch (head->type) {
+            case ULTRA_ATTRIBUTE_PLATFORM_INFO:
+                platform_info_attrb = kmalloc(head->size);
+                memcpy(platform_info_attrb, head, head->size);
+                break;
+            case ULTRA_ATTRIBUTE_KERNEL_INFO:
+                kernel_info_attrb = kmalloc(head->size);
+                memcpy(kernel_info_attrb, head, head->size);
+                break;
+            case ULTRA_ATTRIBUTE_MEMORY_MAP:
+                memory_map = kmalloc(head->size);
+                memcpy(memory_map, head, head->size);
+                break;
+            case ULTRA_ATTRIBUTE_FRAMEBUFFER_INFO:
+                framebuffer = kmalloc(head->size);
+                memcpy(framebuffer, head, head->size);
+                break;
+            default:
+                break;
+        }
+    }
+    cli();
+    pmm_memory_map = memory_map;
     mmu_init_pd(&kernel_page_directory);
+
     mmu_switch_pd(&kernel_page_directory);
+    pmm_reclaim_bootloader_memory();
 
     for(;;) ;
 }

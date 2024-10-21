77120fdf00e37a2215af1d56be1b6d04c999b6d1
NotBonzo <145292897+notbonzo-com@users.noreply.github.com>
2024-10-21 17:29:13 +0200

    Fixed mutex lock again because git rolled back my changees

--- kernel/src/proc/mutex.c
+++ kernel/src/proc/mutex.c
@@ -6,22 +6,17 @@ void mutex_lock(struct Mutex* mutex) {
     while (__sync_lock_test_and_set(&mutex->lock, 1)) {
         yield();
     }
-    mutex->owner_pid = current_job->pid;
 }
 
 bool mutex_trylock(struct Mutex* mutex) {
     if (__sync_lock_test_and_set(&mutex->lock, 1) == 0) {
-        struct JCB* current_job = current_job;
         return true;
     }
     return false;
 }
 
 void mutex_unlock(struct Mutex* mutex) {
-    if (mutex->owner_pid == current_job->pid) {
-        mutex->owner_pid = 0;
-        __sync_lock_release(&mutex->lock);
-    }
+    __sync_lock_release(&mutex->lock);
 }
 
 bool mutex_is_locked(struct Mutex* mutex) {

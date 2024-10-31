63ee5902cb693249e114bd77299c186fc6d8c282
NotBonzo <145292897+notbonzo-com@users.noreply.github.com>
2024-10-31 18:49:44 +0100

    VFS Interface designed

---
+++ kernel/inc/proc/vfs.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+enum vnode_type {
+    VNODE_FILE,
+    VNODE_DIR,
+    VNODE_BLOCK_DEVICE,
+    VNODE_CHAR_DEVICE,
+    VNODE_SYMLINK,
+    VNODE_SOCKET,
+    VNODE_BAD
+};
+
+struct vfs_operations {
+    int (*mount)(struct vfs* vfs, const char* path, struct vnode* mountat);
+    int (*unmount)(struct vfs* vfs);
+    int (*groot)(struct vfs* vfs, struct vnode** root); // root will be filled with a pointer to the root node
+    int (*sync)(struct vfs* vfs);
+    int (*to_fd)(struct vfs* vfs, struct vnode* vnode, uint32_t flags, uint64_t* fd);
+    int (*from_fd)(struct vfs* vfs, uint64_t fd, uint32_t flags, struct vnode** vnode);
+};
+
+struct vfs {
+    uintptr_t vfs_data;                     // Filesystem dependant data
+    struct vfs_operations* vfs_op;          // Standartised functions to operate on the filesystem
+    struct vnode* mount;                    // Where the filesystem is mounted, nullptr for root vfs and non-mounted filesystems
+
+    uint16_t flags;                         // VFS flags
+    uint16_t block_size;                    // VFS block size, such as 512 for fdc and ata devices
+
+    struct vfs* next;
+};
+
+struct vnode_operations {
+    int (*open)(struct vnode* vnode, const char* name, uint32_t flags, struct vnode** out);
+    int (*close)(struct vnode* vnode);
+    int (*rw)(struct vnode* vnode, uint8_t* buffer, size_t len, uint32_t flags, bool write);
+    int (*access)(struct vnode* vnode, uint32_t mode);
+    int (*create)(struct vnode* vnode, const char* name, uint32_t mode, uint32_t flags);
+    int (*remove)(struct vnode* vnode, const char* name);
+    int (*rename)(struct vnode* vnode, const char* oldname, const char* newname);
+    int (*mkdir)(struct vnode* vnode, const char* name, uint32_t mode);
+    int (*rmdir)(struct vnode* vnode, const char* name);
+    int (*readdir)(struct vnode* vnode, const char** names, size_t* count);
+
+    int (*link)(struct vnode* vnode, const char* name, struct vnode* target);
+    int (*unlink)(struct vnode* vnode);
+    int (*symlink)(struct vnode* vnode, const char* name, uint32_t flags, struct vnode** out); // like open() but if used on a symlink gets what it points to.
+    int (*fsync)(struct vnode* vnode); // like sync() but for a specific vnode
+    
+    int (*ioctl)(struct vnode* vnode, uint32_t request, void* arg);
+};
+
+struct vnode {
+    enum vnode_type type;
+    struct vfs* vfs;
+
+    uint16_t slocks;                        // For flock()'s shared locks
+    uint16_t elocks;                        // For flock()'s exclusive locks
+    uint16_t refcount;
+
+    struct vfs* mount;                      // If there is a filesystem mounted over me, this is it.
+    struct vnode_operations* vnode_op;      // Standartised functions to operate on the vnode
+};
+
+extern struct vfs* root_vfs;
+
+bool vfs_initroot();
+
+bool vfs_mount(struct vfs* fs, struct vnode* node);
+bool vfs_unmount(struct vnode* node);
+
+int vfs_resolve_path(const char* path, struct vnode** out_vnode);
+int vfs_dup_fd(struct vfs* vfs, uint64_t fd, uint64_t* new_fd);
+int vfs_close_fd(struct vfs* vfs, uint64_t fd);
+
+int vfs_flock(struct vnode* vnode, uint16_t lock_type);
+int vfs_sync_all();
+
+int vfs_walk(struct vnode* vnode, const char* path, struct vnode** result);
+
